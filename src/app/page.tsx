75fdc34c2b62e7b357f9a8ff6c163a849cd10c50
Leoozito <leonardonogueira616@gmail.com>
2023-06-15 09:02:12 -0300

    Iniciando novo aprendizado da melhor ferramenta de design frontend

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div className="text-4xl text-blue-800 font-bold p-6 bg-red-200 w-56 h-32 sm-h56">
+          <h1>Leonardo</h1>
+        </div>
+    );
+}
+
+export default App;
