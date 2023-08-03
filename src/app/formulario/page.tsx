1f42117f4bd3f15bce4b8d1737beba5e6b147134
Leoozito <leonardonogueira616@gmail.com>
2023-08-03 16:09:36 -0300

    feat: :fire: [UPD] Responsividades
    Formulario responsivo
    
    Diversas maneiras de deixar um projeto responsivo - treino

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -65,6 +65,98 @@ export default function Empresa_Nf() {
                     <Input disabled={desabilitarInput} label="Inscrição Estadual:" />
                 </div>
               </div>
+
+              <div className="mt-5 grid lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 gap-6">
+                    <div className='grid md:grid-cols-1 xl:grid-cols-1 lg:grid-cols-1 2xl:grid-cols-2 gap-6'>
+                        <div>
+                            <Select label="Regime Tributário:" disabled={desabilitarInput}>
+                                <Option className="hidden md:block">NENHUM</Option>
+                                <Option className="hidden md:block">Simples Nacional</Option>
+                                <Option className="hidden md:block">Simples Nacional - Excesso</Option>
+                                <Option className="hidden md:block">Normal - Lucro Prezumido</Option>
+                                <Option className="hidden md:block">Normal - Lucro Real</Option>
+                            </Select>
+                        </div>
+                        <div className='mr-1'>
+                            <Select label="Regime Tributário Especial:" disabled={desabilitarInput}>
+                                <Option className="hidden md:block">Sem Regime Tributário Especial</Option>
+                                <Option className="hidden md:block">Micro Empresa Municipal</Option>
+                                <Option className="hidden md:block">Estimativa</Option>
+                                <Option className="hidden md:block">Sociedade de Profissionais</Option>
+                                <Option className="hidden md:block">Cooperativa</Option>
+                                <Option className="hidden md:block">Microempresário Individual - MEI</Option>
+                                <Option className="hidden md:block">Microempresa ou Pequena Porte - ME EPP</Option>
+                            </Select>
+                        </div>
+                    </div>
+                    <div className='grid xl:grid-cols-1 2xl:grid-cols-3 gap-6 xl:ml-3'>
+                        <div className="">
+                            <Select label="Incentivo fiscal:" disabled={desabilitarInput}>
+                            <Option className="hidden md:block">Sim</Option>
+                            <Option className="hidden md:block">Não</Option>
+                            </Select>
+                        </div>
+                        <div className="">
+                            <Select label="Incentivo cultural:" disabled={desabilitarInput}>
+                                <Option className="hidden md:block">Sim</Option>
+                                <Option className="hidden md:block">Não</Option>
+                            </Select>
+                        </div>
+                        <div className=''>
+                            <Select label="Simples Nacional:" disabled={desabilitarInput}>
+                                <Option className="hidden md:block">Sim</Option>
+                                <Option className="hidden md:block">Não</Option>
+                            </Select>
+                        </div>
+                    </div>
+              </div>
+
+              <div className="mt-5 grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-6">
+                  <div className="grid md:grid-cols-1 xl:grid-cols-2 gap-6">
+                      <div className="">
+                          <Input label="NCM :"  disabled={desabilitarInput}/>
+                      </div>
+                      <div className="">
+                          <Select label="CFOP :" disabled={desabilitarInput}>
+                              <Option className="hidden md:block">5101</Option>
+                              <Option className="hidden md:block">5102</Option>
+                          </Select>
+                      </div>
+                  </div>
+                  <div className="grid md:grid-cols-1 xl:grid-cols-2 gap-6 xl:ml-3">
+                      <div className="">
+                          <Input label="ICMS-ORIGEM :" disabled={desabilitarInput}/>
+                      </div>
+                      <div className="">
+                          <Input label="ICMS-CST :" disabled={desabilitarInput}/>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="mt-5 grid lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-2 3xl:grid-cols-2 3xl:flex gap-6">
+                <div className="grid md:grid-cols-1 lg:grid-cols-1 2xl:grid-cols-2 gap-6">
+                    <div className="flex grid-cols-1 md:grid-cols-2">
+                        <Select label="Tipo Logradouro:" disabled={desabilitarInput}>
+                            <Option className="hidden md:block">RUA</Option>
+                            <Option className="hidden md:block">AVENIDA</Option>
+                            <Option className="hidden md:block">ALAMEDA</Option>
+                            <Option className="hidden md:block">TRAVESSA</Option>
+                            <Option className="hidden md:block">PRAÇA</Option>
+                            <Option className="hidden md:block">RODOVIA</Option>
+                            <Option className="hidden md:block">ESTRADA</Option>
+                        </Select>
+                    </div>
+                    <div className=''>
+                        <Input label="Bairro :" disabled={desabilitarInput}/>
+                    </div>
+                </div>
+                <div className='grid lg:grid-cols-1 xl:grid-cols-1 2xl:grid-cols-1 3xl:grid-cols-1 gap-6 xl:ml-3'>
+                    <div className='flex'>
+                        <input placeholder="Número:" className="mr-3 h-10 w-40 border bg-transparent border-gray-400 text-gray-800 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-900 p-2" disabled={desabilitarInput} />
+                        <Input className="w-full" label="Complemento :" disabled={desabilitarInput}/>
+                    </div>
+                </div>
+              </div>
             </CardBody>
           </Card>
         </>
