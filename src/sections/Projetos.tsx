58b7a152051f2ff601d69a18fb982e02687d102c
Leoozito <leonardonogueira616@gmail.com>
2024-02-15 01:23:34 -0300

    feat: :art: GSAP
    Instalando biblioteca GSAP

--- src/sections/Projetos.tsx
+++ src/sections/Projetos.tsx
@@ -7,8 +7,10 @@ export default function Projects() {
             <div className="my-6 justify-center items-center flex mt-16 mb-10">
                 <h1 className="text-4xl font-extrabold text-indigo-900 dark:text-[#818cf8]">Projetos</h1>
             </div>
-            <Card className="justify-center w-auto h-96 ml-60 mr-60 flex dark:bg-[#1e293b]">
-            </Card>
+            {/* <Card 
+                className="justify-center w-auto h-96 ml-60 mr-60 flex dark:bg-[#1e293b]"
+            >
+            </Card> */}
         </>
     )
 }
