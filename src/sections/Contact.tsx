9701b92aaa5edcbd63d9f24b5a28141a58f300d6
Leoozito <leonardonogueira616@gmail.com>
2024-02-20 13:16:30 -0300

    validação

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -3,6 +3,9 @@ import { Card, Input, Button, Textarea, Typography } from "@material-tailwind/re
 import { useState } from "react"
 import emailjs from '@emailjs/browser'
 import Image from "next/image"
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form"
 
 
 export default function Contact() {
@@ -10,6 +13,21 @@ export default function Contact() {
     const [nomeRecrutador, setNomeRecrutador] = useState("")
     const [emailRecrutador, setEmailRecrutador] = useState("")
     const [mensagem, setMensagem] = useState("")
+
+    const createUserFormSchema = z.object({
+        emailRecrutador: z.string()
+          .nonempty("Campo obrigatório!")
+          .email("Formato de email invalido"),
+        nomeRecrutador: z.string()
+          .nonempty("Campo obrigatório!"),
+        mensagem: z.string().nonempty("Campo obrigatório!")
+    })
+
+    type createUserFormData = z.infer<typeof createUserFormSchema>
+
+    const { register, handleSubmit, watch, formState: { errors } } = useForm<createUserFormData>({
+        resolver: zodResolver(createUserFormSchema)
+    });
 
     function enviaEmail() {
 
@@ -59,27 +77,36 @@ export default function Contact() {
 
                 <div className="relative justify-center items-center flex shadow-2xl shadow-indigo-200 xs:-bottom-40 xs:left-0 sm:-bottom-24 sm:right-40 md:-top-6 md:-right-32 lg:-right-44 xs:w-[350px] sm:w-[550px] lg:w-[500px]">
                     <Card className="w-full xs:p-10 items-center">
-                        <form action="" onSubmit={enviaEmail}>
+                        <form action="" onSubmit={handleSubmit(enviaEmail)}>
                             {/* recrutador enviar mensagem   */}
                             <div className="justify-center items-center xs:mx-0 2xl:mx-60 flex sm:p-8">
                                 <div className="justify-center grid grid-cols-1">
                                     <div className="xs:grid sm:flex md:grid lg:flex mb-8 sm:gap-12 xs:gap-6 md:gap-6">
-                                        <Input
-                                            onChange={(e) => setNomeRecrutador(e.target.value)}
-                                            placeholder="Seu Nome"
-                                            value={nomeRecrutador} className="items-center w-auto"
-                                        />
-                                        <Input
-                                            onChange={(e) => setEmailRecrutador(e.target.value)}
-                                            value={emailRecrutador} placeholder="Endereço de email" className="items-center w-auto"
-                                        />
+                                        <div className="flex-col">
+                                            <Input
+                                                {...register("nomeRecrutador")}
+                                                placeholder="Seu Nome"
+                                                value={nomeRecrutador} className="items-center w-auto"
+                                            />
+                                            {errors.nomeRecrutador && <span>{errors.nomeRecrutador.message}</span>}
+                                        </div>
+                                        <div className="flex-col">
+                                            <Input
+                                                {...register("emailRecrutador")}
+                                                value={emailRecrutador} placeholder="Endereço de email" className="items-center w-auto"
+                                            />
+                                            {errors.emailRecrutador && <span>{errors.emailRecrutador.message}</span>}
+                                        </div>
                                     </div>
                                     <div>
-                                        <Textarea
-                                            onChange={(e) => setMensagem(e.target.value)}
-                                            value={mensagem}
-                                            placeholder="Sua mensagem" className="xs:h-24 xs:w-64 sm:h-32 sm:w-full mb-6 items-center"
-                                        />
+                                        <div className="flex-col">
+                                            <Textarea
+                                                {...register("mensagem")}
+                                                value={mensagem}
+                                                placeholder="Sua mensagem" className="xs:h-24 xs:w-64 sm:h-32 sm:w-full mb-6 items-center"
+                                            />
+                                            {errors.mensagem && <span>{errors.mensagem.message}</span>}
+                                        </div>
                                         <Button
                                             onClick={enviaEmail} className="justify-center items-center flex xs:w-24 xs:h-8 sm:w-32 md:w-48"
                                         >
