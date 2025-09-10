7c34d5595c8c12133e4869612810826dd7a14505
Leoozito <leonardonogueira616@gmail.com>
2025-09-09 23:31:57 -0300

    feat: :bug: Ultimo ajuste de pacotes, troca Material Tailwind por MUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {(tailwindConfig: object) => object} */
 
-const withMT = require("@material-tailwind/react/utils/withMT");
+const withMT = require("@mui/material/utils/withMT");
 
 module.exports = withMT({
   darkMode: "class",
@@ -11,8 +11,8 @@ module.exports = withMT({
 
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@mui/material/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@mui/material/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
